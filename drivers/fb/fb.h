2d21cd02697313e9798666f8bde4c95657c3ded1
Willian Oliveira <williandossantosdeoliveira287@gmail.com>
2024-07-07 22:54:18 -0300

    Add foreground color for characters

--- drivers/fb/fb.h
+++ drivers/fb/fb.h
@@ -2,7 +2,10 @@
 #define _include_fb_
 
 #include <ant/boot.h>
+#include <ant/types.h>
 
+void fb_set_background_color(u32);
+void fb_set_foreground_color(u32);
 void fb_init(struct boot_info *info);
 void fb_write(char *);
 
