34abc8310ecef3b4f66ad09f51b73002e1d5afe0
Willian Oliveira <williandossantosdeoliveira287@gmail.com>
2024-11-05 22:17:57 -0300

    Improve kprintf

--- libk/kprintf.c
+++ libk/kprintf.c
@@ -3,7 +3,31 @@
 #include <fb/fb.h>
 #include <stdlib.h>
 
-static void print_int(u64 number)
+static void print_hex(u64 number, _Bool uppercase)
+{
+  u8 nibble;
+  _Bool zero_num = 0;
+  u64 i;
+  while(number)
+  {
+    nibble = number >> 60 & 0xf;
+    number <<= 4;
+ 
+    if(!nibble && !zero_num)
+      continue;
+    else
+      zero_num = 1;
+
+    if(nibble < 0xa)
+      fb_put_char(nibble + '0');
+    else if(uppercase)
+      fb_put_char(nibble - 10 + 'A');
+    else
+      fb_put_char(nibble - 10 + 'a');
+  }
+}
+
+static void print_int(s32 number)
 {
   char string[22] = {0};
   itoa(string, number);
@@ -33,6 +57,22 @@ int kprintf(const char *format, ...)
           break;
         case 'i':
           print_int(va_arg(arg, int));
+          break;
+        case 'X':
+          print_hex(va_arg(arg, int), 1);
+          break;
+        case 'x':
+          print_hex(va_arg(arg, int), 0);
+          break;
+        case '#':
+          format++;
+          fb_write("0x");
+          if(*format == 'X')
+            print_hex(va_arg(arg, int), 1);
+          else if(*format == 'x')
+            print_hex(va_arg(arg, int), 0);
+
+          break;
        }
     }
     else
