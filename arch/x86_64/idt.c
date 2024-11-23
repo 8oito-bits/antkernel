81e7a83077b914e147424ceb390a45ab87e2b6b4
Willian Oliveira <williandossantosdeoliveira287@gmail.com>
2024-11-23 18:11:27 -0300

    Fix idt entry and remove handler()

--- arch/x86_64/idt.c
+++ arch/x86_64/idt.c
@@ -7,15 +7,11 @@ struct idt_register idtr;
 extern void idtr_set(u64);
 extern void exception_handler(void);
 
-void handler(void)
-{
-}
-
 void idt_load(void)
 {
   u32 i;
   for(i = 0; i < 33; i++)
-    idt[i] = (struct idt_entry) IDT_ENTRY((u64) exception_handler - 0xFFFFFFFF80000000UL, 0x8, 0x0, 0xe, 0x0, 0x1);
+    idt[i] = (struct idt_entry) IDT_ENTRY((u64) exception_handler, 0x8, 0x0, 0xe, 0x0, 0x1);
   idtr.limit = sizeof(idt) - 1;
   idtr.base = (u64) idt;
 
