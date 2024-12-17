049751cab536babc760ed1cb3518a83e71938a95
Willian Oliveira <williandossantosdeoliveira287@gmail.com>
2024-12-16 22:12:01 -0300

    Fill idt entries with handler exceptions

--- arch/x86_64/idt.c
+++ arch/x86_64/idt.c
@@ -1,5 +1,6 @@
 #include <idt.h>
 #include <fb/fb.h>
+#include <exception.h>
 
 struct idt_entry idt[IDT_ENTRIES];
 struct idt_register idtr;
@@ -12,7 +13,7 @@ extern void exception_handler(void);
 void idt_load(void)
 {
   int i;
-  for(i = 0; i < 2; i++)
+  for(i = 0; i < NUMBER_OF_EXCEPTIONS; i++)
     idt[i] = (struct idt_entry) IDT_ENTRY((u64) (&ex_handlers)[i], 0x8, 0x0, 0xe, 0x0, 0x1);
 
   idtr.limit = sizeof(idt) - 1;
--- arch/x86_64/include/exception.h
+++ arch/x86_64/include/exception.h
@@ -1,6 +1,8 @@
 #ifndef _INCLUDE_EXCEPTION_
 #define _INCLUDE_EXCEPTION_
 
+#define NUMBER_OF_EXCEPTIONS 21
+
 void exception_handler_c(unsigned long); 
 
 #endif
